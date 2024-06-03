f12774644f9755b34e4505b734109bae7b3e35e8
Yudann <yudannnnn80@gmail.com>
2024-06-03 14:00:17 +0700

    initial commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,7 +7,6 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
-https://github.com/Yudann/portfolio-website.git
 
 function Contact() {
   return (
